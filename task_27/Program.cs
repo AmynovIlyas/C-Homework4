8769a34e79010f3ad3afbbc96eecb3afc62a6410
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-05-24 23:00:18 +0300

    Solution oi\f the task 27

---
+++ task_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt()
+{
+    Console.WriteLine("Введите число: ");
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int SumNumber(int num)
+{
+    int result = 0;
+    while(num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+int number = Prompt();
+Console.WriteLine($"{number} --> {SumNumber(number)}");
